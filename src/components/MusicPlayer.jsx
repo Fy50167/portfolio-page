5becdb12ff1b5ad46078a4555e32d87310cc9f6c
Francis Yang <fy50167@gmail.com>
2024-01-06 05:34:08 -0800

    add onTimeUpdate handler for audio tag

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,6 +1,4 @@
-import ASHES from '../assets/Ashes to Ashes (Mastered).mp3';
-import DISCONNECT from '../assets/disConnect.mp3';
-import DANCE from '../assets/Moonlight Dance (Mastered).mp3';
+import { songlist } from '../utils/audio';
 import AVATAR from '../assets/KD_Avatar_Circle.png';
 import PLAY from '../assets/play.png';
 import PAUSE from '../assets/pause.png';
@@ -9,8 +7,6 @@ import BACK from '../assets/back.png';
 import MUSIC from '../assets/music.png';
 import CLOSE from '../assets/close.png';
 import { useEffect, useState, useRef } from 'react'; 
-const songs = [ASHES, DISCONNECT, DANCE];
-const songTitles = ['Ashes to Ashes', 'disConnect', 'Moonlight Dance'];
 
 function MusicPlayer() {
 
@@ -18,6 +14,8 @@ function MusicPlayer() {
     const [isCollapsed, setIsCollapsed] = useState(true);
     const [duration, setDuration] = useState(0);
     const [currentTime, setCurrentTime] = useState(0);
+    const [songs, setSongs] = useState(songlist);
+    const [currentSong, setCurrentSong] = useState(songlist[0]);
 
     const musicPlayer = useRef();
     const progressBar = useRef();
@@ -62,7 +60,7 @@ function MusicPlayer() {
             </div>
           :
           <div className = 'position-fixed d-flex flex-column justify-content-evenly align-items-center music-player open py-3'>
-            <audio ref = {musicPlayer} src = {ASHES} preload = "metadata" onLoadedMetadata = {() => setDuration(Math.floor(musicPlayer.current.duration))}></audio>
+            <audio ref = {musicPlayer} src = {currentSong.url} onLoadedmetadata = {() => setDuration(Math.floor(musicPlayer.current.duration))} onTimeUpdate={() => setCurrentTime(Math.floor(musicPplayer.current.currentTime))}></audio>
             <img className = 'close position-absolute mouse-pointer' src = {CLOSE} onClick = {expandCollapse} />
             <h2 className = 'stylized'>Music Player</h2>
             <img src = {AVATAR} alt = 'avatar' className = 'avatar' />
---
+++ src/utils/audio.js
@@ -0,0 +1,18 @@
+import ASHES from '../assets/Ashes to Ashes (Mastered).mp3';
+import DISCONNECT from '../assets/disConnect.mp3';
+import DANCE from '../assets/Moonlight Dance (Mastered).mp3';
+
+exports.songlist = [
+    {
+        "title": "Ashes to Ashes",
+        "url": {ASHES}
+    },
+    {
+        "title": "disConnect",
+        "url": {DISCONNECT}
+    },
+    {
+        "title": "Moonlight Dance",
+        "url": {DANCE}
+    }
+]
